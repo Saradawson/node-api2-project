e2d7cb116e55fa82a1d06be501536e5efbf465ae
unknown <saradawsone@gmail.com>
2022-10-14 18:52:37 -0600

    router finding post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,7 +17,23 @@ router.get('/', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    Post.findById(id)
+        .then(post =>{
+            if(!post){
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist" 
+                })
+            }else{
+                res.status(200).json(post);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post information could not be retrieved",
+                err: err.message
+            })
+        })
 })
 
 router.post('/', (req, res) => {
