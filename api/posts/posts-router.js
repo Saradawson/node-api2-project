43456f0d978db21070b234b3141f4477f34f6dd9
unknown <saradawsone@gmail.com>
2022-10-16 13:14:29 -0600

    router is getting comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -116,7 +116,26 @@ router.put('/:id', (req, res) => {
 })
 
 router.get('/:id/comments', (req, res) => {
-
+    const { id } = req.params
+    Post.findById(id)
+        .then(post =>{
+            if(!post){
+               res.status(404).json({
+                message: "The post with the specified ID does not exist"
+               }) 
+            }else{
+                return Post.findPostComments(id)
+            }
+        })
+        .then(comments => {
+            res.status(200).json(comments)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The comments information could not be retrieved",
+                err: err.message
+            })
+        })
 })
 
 module.exports = router;
